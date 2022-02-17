e1c9d0c0b0127fce3f397a1f635a72ab753c86f5
starlight258 <audwl03071@gmail.com>
2022-02-17 16:09:56 +0900

    final

--- exporter.py
+++ exporter.py
@@ -4,7 +4,7 @@ import csv
 def save_to_file(jobs):
     file = open("jobs.csv", mode="w")  # 쓰기모드로 파일열기
     writer = csv.writer(file)  # 쓸 파일 지정
-    writer.writerow(["title", "company", "location", "link"])
+    writer.writerow(["Title", "Company", "Location", "Link"])
     for job in jobs:
         writer.writerow(list(job.values()))  # 딕셔너리 값을 리스트로 가져오기
     return
